b7fed236e944174da34484305bfb5c7111a263c1
Mike Fix <mrfix84@gmail.com>
2018-12-19 23:47:16 -0600

    add TODO

--- src/issue/labeled.js
+++ src/issue/labeled.js
@@ -20,6 +20,7 @@ module.exports = queue => async context => {
 
   const config = await getConfig(context)
 
+  // TODO check for action === undefined or 'close' here
   const withClosableLabels = thread.labels.filter(l => !!config.labels[l.name])
 
   if (withClosableLabels.length) {
