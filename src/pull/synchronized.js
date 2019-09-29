ad9e543fe47a59eda83bca2dc18607c24340b7aa
Mike Fix <mrfix84@gmail.com>
2019-09-29 15:34:31 -0700

    use ref instead of commit_sha parameter

--- src/pull/synchronized.js
+++ src/pull/synchronized.js
@@ -34,7 +34,7 @@ module.exports = () => async context => {
     data: {
       commit: { message: body }
     }
-  } = await context.github.repos.getCommit(context.repo({ commit_sha: sha }))
+  } = await context.github.repos.getCommit(context.repo({ ref: sha }))
 
   // TODO confirm this API before releasing in docs
   const rules = config.commits
