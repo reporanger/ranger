389dacd583f7212796dfd7c97945fd10721db199
Mike Fix <mrfix84@gmail.com>
2020-05-07 21:29:40 -0700

    prettier-ish

--- scripts/write-installations-to-segment.js
+++ scripts/write-installations-to-segment.js
@@ -7,21 +7,21 @@ const t = require('../save/create-token')
 const s = new Sema(
   1, // Allow 4 concurrent async calls
   {
-    capacity: 100 // Prealloc space for 100 tokens
+    capacity: 100, // Prealloc space for 100 tokens
   }
 )
 
 async function fetchData(login) {
   await s.acquire()
   try {
-    await new Promise(res => setTimeout(res, 10000))
+    await new Promise((res) => setTimeout(res, 20000))
     return await fetch(`https://api.github.com/users/${login}`, {
       method: 'GET',
       headers: {
         accept: 'application/vnd.github.machine-man-preview+json',
-        'user-agent': 'octokit.js/16.28.1 Node.js/10.16.0 (macOS Mojave; x64)'
-      }
-    }).then(res => res.json())
+        'user-agent': 'octokit.js/16.28.1 Node.js/10.16.0 (macOS Mojave; x64)',
+      },
+    }).then((res) => res.json())
   } finally {
     s.release()
   }
@@ -32,13 +32,13 @@ fetch('https://api.github.com/app/installations?per_page=100', {
   headers: {
     accept: 'application/vnd.github.machine-man-preview+json',
     'user-agent': 'octokit.js/16.28.1 Node.js/10.16.0 (macOS Mojave; x64)',
-    authorization: `Bearer ${t}`
-  }
+    authorization: `Bearer ${t}`,
+  },
 })
-  .then(res => res.json())
-  .then(async installations => {
+  .then((res) => res.json())
+  .then(async (installations) => {
     return Promise.all(
-      installations.map(async i => {
+      installations.map(async (i) => {
         const user = await fetchData(i.account.login)
         const email = user.email
         console.log(user)
@@ -49,8 +49,8 @@ fetch('https://api.github.com/app/installations?per_page=100', {
             name: i.account.login,
             username: i.account.login,
             type: i.account.type,
-            email
-          }
+            email,
+          },
         })
       })
     )
