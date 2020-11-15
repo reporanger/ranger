604d87b1f6168426b17cbe94f4191dc7aed6f774
Michael Fix <mrfix84@gmail.com>
2020-11-14 17:39:21 -0800

    Revert "Revert "Upgrade probot"" (#97)
    * Revert "Revert "Upgrade probot (#95)" (#96)"
    
    This reverts commit 395b44fa2ca30ccdc4f99b7aa57c47f519ee0442.
    
    * regenerate lock file

--- index.js
+++ index.js
@@ -22,15 +22,16 @@ const verifyPaymentPlan = require('./src/verify-payment-plan')
 // Probot will also send errors to Sentry DNS: https://probot.github.io/docs/configuration/
 Sentry.init({ dsn: process.env.SENTRY_DSN })
 
-module.exports = async (robot) => {
-  robot.route('/').get('/health', (req, res) => res.send('OK'))
+module.exports = async ({ app, getRouter }) => {
+  const router = getRouter('/')
+  router.get('/health', (req, res) => res.send('OK'))
 
   if (process.env.NODE_ENV !== 'test') {
     process.on('uncaughtException', (e) => {
-      robot.log.error(e)
+      app.log.error(e)
     })
     process.on('unhandledRejection', (e) => {
-      robot.log.error(e)
+      app.log.error(e)
     })
   }
 
@@ -53,30 +54,30 @@ module.exports = async (robot) => {
     try {
       switch (job.data.action) {
         case COMMENT:
-          return await threadLabeled.process(robot)(job)
+          return await threadLabeled.process(app)(job)
         case MERGE:
-          return await pullLabeled.process(robot)(job)
+          return await pullLabeled.process(app)(job)
         case CLOSE:
         default:
-          return await issueLabeled.process(robot)(job)
+          return await issueLabeled.process(app)(job)
       }
     } catch (error) {
       if (process.env.NODE_ENV !== 'test') {
-        robot.log(error, job)
+        app.log(error, job)
       }
       throw error
     }
   })
 
   queue.on('succeeded', (job, result) => {
-    robot.log.debug(`Job ${job.id} succeeded with result: ${JSON.stringify(result, null, 2)}`)
+    app.log.debug(`Job ${job.id} succeeded with result: ${JSON.stringify(result, null, 2)}`)
   })
 
   queue.on('failed', (job, err) => {
     if (err.message === 'Retry job') {
       return
     }
-    robot.log.error(
+    app.log.error(
       `Job ${job.id} with data ${JSON.stringify(job.data, null, 2)} failed with error ${
         err.message
       }`
@@ -91,23 +92,23 @@ module.exports = async (robot) => {
 
   function wrapPaymentCheck(fn) {
     return async (context) => {
-      if (await verifyPaymentPlan(robot, context)) {
+      if (await verifyPaymentPlan(app, context)) {
         fn(context)
       }
     }
   }
 
   // Listeners
-  robot.on(
+  app.on(
     // All pull requests are issues in GitHub REST V3
     ['issues.labeled', 'issues.unlabeled', 'pull_request.labeled', 'pull_request.unlabeled'],
     wrapPaymentCheck(threadLabeled(queue))
   )
-  robot.on(['issues.opened', 'pull_request.opened'], wrapPaymentCheck(threadOpened(queue)))
+  app.on(['issues.opened', 'pull_request.opened'], wrapPaymentCheck(threadOpened(queue)))
 
-  robot.on(['issues.labeled', 'issues.unlabeled'], wrapPaymentCheck(issueLabeled(queue)))
+  app.on(['issues.labeled', 'issues.unlabeled'], wrapPaymentCheck(issueLabeled(queue)))
 
-  robot.on(
+  app.on(
     [
       'pull_request.labeled',
       'pull_request.unlabeled',
@@ -119,23 +120,20 @@ module.exports = async (robot) => {
   )
   // TODO rerun pull labeled job on `check_suite.completed`
 
-  robot.on(['issue_comment.created', 'issue_comment.edited'], wrapPaymentCheck(commentCreated()))
+  app.on(['issue_comment.created', 'issue_comment.edited'], wrapPaymentCheck(commentCreated()))
 
-  robot.on('pull_request.closed', wrapPaymentCheck(pullMerged.deleteBranch()))
-  robot.on('pull_request.closed', wrapPaymentCheck(pullMerged.createTag()))
+  app.on('pull_request.closed', wrapPaymentCheck(pullMerged.deleteBranch()))
+  app.on('pull_request.closed', wrapPaymentCheck(pullMerged.createTag()))
 
-  robot.on(
-    ['pull_request.opened', 'pull_request.synchronize'],
-    wrapPaymentCheck(pullSynchronized())
-  )
+  app.on(['pull_request.opened', 'pull_request.synchronize'], wrapPaymentCheck(pullSynchronized()))
 
   // Kill job when issue/pull is closed
-  robot.on(['issues.closed', 'pull_request.closed'], threadClosed(queue))
+  app.on(['issues.closed', 'pull_request.closed'], threadClosed(queue))
 
-  robot.on('issue_comment.deleted', commentDeleted(queue))
+  app.on('issue_comment.deleted', commentDeleted(queue))
 
-  robot.on(['installation.created'], installationCreated(robot))
-  robot.on(['installation_repositories.added', 'installation.created'], installationAdded(robot))
+  app.on(['installation.created'], installationCreated(app))
+  app.on(['installation_repositories.added', 'installation.created'], installationAdded(app))
   // TODO 'marketplace_purchase.purchased'
 
   // TODO use status updates to retrigger merge jobs
@@ -147,6 +145,6 @@ module.exports = async (robot) => {
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 
-  robot.queue = queue
-  robot.analytics = analytics
+  app.queue = queue
+  app.analytics = analytics
 }
